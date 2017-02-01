ed4a3cba30fce179e6c99e5e3307e2c49c2dcb62
Sisi Li <vivianss0301@gmail.com>
2017-02-01 00:08:17 -0500

    updated server.js and package.json

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const express = require('express');
 
 const app = express();
 
-
-
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
 
-app.use('/hello', (req, res, next) =>{
- 
+
+
+
+// second route is'/hello'
+app.use('/hello', (req, res, next) =>{ 
   res.send("Hello World!");
  
 });
 
+//first route is '/' - root of my website
 app.use('/', (req, res, next) =>{
   
   res.send("Welcome!");
-  
-  
 });
 
 module.exports = app;
